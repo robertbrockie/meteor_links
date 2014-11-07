2567217a9ff2891bc91bdf4ffc98bd538e553d3e
Robert Brockie <robertbrockie@gmail.com>
2014-11-06 22:22:01 -0500

    Quick template.

--- meteor_links.js
+++ meteor_links.js
@@ -1,17 +1,9 @@
 if (Meteor.isClient) {
   // counter starts at 0
-  Session.setDefault("counter", 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get("counter");
-    }
-  });
-
-  Template.hello.events({
+  Template.link_input.events({
     'click button': function () {
-      // increment the counter when button is clicked
-      Session.set("counter", Session.get("counter") + 1);
+      var url = document.getElementById("url").value;
+      alert('url: ' + url);
     }
   });
 }
